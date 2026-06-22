9bf75748a0daa0d19d67f1f92838f2e7987ba766
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-22 13:22:34 -0600

    made list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 
+/*
+function getTitle(title){
+  return title
+}
+
+const welcome = {
+  greeting: "hello",
+  title: "alejandro",
+};
+*/
+
+const list=[
+  {
+    title:'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title:'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
 function App() {
-
-  //this is a function that acceptes parameters and returns html
   return (
-    <div>
-      <h1>Hello World!</h1>
+
+    <div>      
+      <h1>My hacker stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+
+      <hr/>
+
+      <ul>
+        {
+          list.map(
+            function(item){
+              return <li>{item.title}</li>
+            }
+          )
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
