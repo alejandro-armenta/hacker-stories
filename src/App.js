31caf72e6547f9182d47af49ee6e0f47301ec529
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-22 14:39:31 -0600

    made components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 
-/*
-function getTitle(title){
-  return title
+
+
+function App() {
+  return (
+
+    <div>
+      <h1>My hacker stories Alejandro Armenta</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <List />
+
+    </div>
+  )
 }
 
-const welcome = {
-  greeting: "hello",
-  title: "alejandro",
-};
-*/
-
-const list=[
+const list = [
   {
-    title:'React',
+    title: 'React',
     url: 'https://reactjs.org/',
     author: 'Jordan Walke',
     num_comments: 3,
@@ -20,7 +28,7 @@ const list=[
     objectID: 0,
   },
   {
-    title:'Redux',
+    title: 'Redux',
     url: 'https://redux.js.org/',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
@@ -29,26 +37,42 @@ const list=[
   },
 ]
 
-function App() {
+function List() {
   return (
+    <ul>
+      {
+        list.map(
+          function (item) {
+            return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>
+                  {item.author}
+                </span>
+                <span>
+                  {item.num_comments}
+                </span>
+                <span>
+                  {item.points}
+                </span>
+              </li>
+            )
+          }
+        )
+      }
+    </ul>
+  )
+}
 
-    <div>      
-      <h1>My hacker stories</h1>
+function Search() {
+  return (
+    <div>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>
+      <input id="search" type="text" />
 
-      <hr/>
-
-      <ul>
-        {
-          list.map(
-            function(item){
-              return <li>{item.title}</li>
-            }
-          )
-        }
-      </ul>
     </div>
   )
 }
