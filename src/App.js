ffa7cf10c08e0e98f314cc44efcd8be497404750
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-22 21:56:48 -0600

    made uodate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 
-//client side rendering
-//server side rendering
-
 import React from "react"
 
 const App = () => {
@@ -22,17 +19,18 @@ const App = () => {
       num_comments: 2,
       points: 5,
       objectID: 1,
-    },
+    }
   ]
 
-  const [searchTerm, setSearchTerm] = React.useState('')
+  //esta de aqui
+  const [searchTerm, setSearchTerm] = React.useState('React')
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value)
   }
 
   const storiesFiltered = stories.filter((story) =>
-      story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
   )
 
   return (
@@ -40,7 +38,7 @@ const App = () => {
 
       <h1>My hacker stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
@@ -49,8 +47,6 @@ const App = () => {
     </div>
   )
 }
-
-
 
 const List = (props) => {
 
@@ -87,17 +83,24 @@ const Item = (props) => {
 }
 
 const Search = (props) => {
-  return (
+  
+  const {search, onSearch} = props
 
+  return (
     <div>
 
       <label htmlFor="search">Search: </label>
 
-      <input id="search" type="text" onChange={props.onSearch} />
+      <input
+        id="search"
+        type="text"
+        value={search}
+        onChange={onSearch}
+      />
 
     </div>
-  );
-};
+  )
+}
 
 
-export default App;
+export default App
