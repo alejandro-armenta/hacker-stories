4594722d24182d43650ad3b932e8769c3542ed2c
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-23 18:19:11 -0600

    erasing element

--- src/App.js
+++ src/App.js
@@ -90,16 +90,23 @@ const List = ({ list, onRemoveItem }) => (
   </ul>
 )
 
-const Item = ({ item }) => (
-  <li>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </li>
-)
+const Item = ({ item, onRemoveItem }) => {
+
+  const handle = () => {
+    onRemoveItem(item)
+  }
+
+  return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  )
+}
 
 const InputWithLabel = (
   {
